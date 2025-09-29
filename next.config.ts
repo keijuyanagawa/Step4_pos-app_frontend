32d2ab42386826cb1fef6f7088f714cf53c28f92
keijuyanagawa <kyanagawa999@gmail.com>
2025-09-30 07:18:49 +0900

    next.config.ts, package.jsonの修正

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Azure App Service用の設定
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-  // ポート設定
-  env: {
-    PORT: process.env.PORT || '8080',
-  },
+  serverExternalPackages: [],
 };
 
 export default nextConfig;
